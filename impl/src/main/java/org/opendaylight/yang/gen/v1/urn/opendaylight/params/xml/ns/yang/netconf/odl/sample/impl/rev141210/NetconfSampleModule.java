b3f58dca29f267a353fc7f2471c99d1e61349a98
Maros Marsalek <mmarsale@cisco.com>
2016-02-06 14:09:12 +0100

    Initial empty ODL app
    Signed-off-by: Maros Marsalek <mmarsale@cisco.com>

---
+++ impl/src/main/java/org/opendaylight/yang/gen/v1/urn/opendaylight/params/xml/ns/yang/netconf/odl/sample/impl/rev141210/NetconfSampleModule.java
@@ -0,0 +1,31 @@
+package org.opendaylight.yang.gen.v1.urn.opendaylight.params.xml.ns.yang.netconf.odl.sample.impl.rev141210;
+
+import org.mars.kotlin.impl.NetconfSampleProvider;
+
+public class NetconfSampleModule extends
+        org.opendaylight.yang.gen.v1.urn.opendaylight.params.xml.ns.yang.netconf.odl.sample.impl.rev141210.AbstractNetconfSampleModule {
+    public NetconfSampleModule(org.opendaylight.controller.config.api.ModuleIdentifier identifier,
+                               org.opendaylight.controller.config.api.DependencyResolver dependencyResolver) {
+        super(identifier, dependencyResolver);
+    }
+
+    public NetconfSampleModule(org.opendaylight.controller.config.api.ModuleIdentifier identifier,
+                               org.opendaylight.controller.config.api.DependencyResolver dependencyResolver,
+                               org.opendaylight.yang.gen.v1.urn.opendaylight.params.xml.ns.yang.netconf.odl.sample.impl.rev141210.NetconfSampleModule oldModule,
+                               java.lang.AutoCloseable oldInstance) {
+        super(identifier, dependencyResolver, oldModule, oldInstance);
+    }
+
+    @Override
+    public void customValidation() {
+        // add custom validation form module attributes here.
+    }
+
+    @Override
+    public java.lang.AutoCloseable createInstance() {
+        final NetconfSampleProvider netconfSampleProvider = new NetconfSampleProvider();
+        getBrokerDependency().registerProvider(netconfSampleProvider);
+        return netconfSampleProvider;
+    }
+
+}
